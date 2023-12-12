75de28e5ffcd57d0338c84b22edb3369aa0caea7
Figo <milkylot@gmail.com>
2023-12-12 19:33:15 +0700

    feat: add complete exercise

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,8 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; //True
+null == undefined; //True
+10 == "10";//True
+[10] == 10;//true
+[1,2] == "1,2";//True
+
